42e3e00fb836e888743eb749eb165a436b3b242e
zhangyibo <goldeneyezhang@163.com>
2020-02-05 14:05:02 +0800

    first commit

---
+++ src/com/yibo/DeepSearch.java
@@ -0,0 +1,4 @@
+package com.yibo;
+
+public class DeepSearch {
+}
---
+++ src/com/yibo/Main.java
@@ -0,0 +1,13 @@
+package com.yibo;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //八数码问题
+        int[] start = new int[]{2,0,3,1,8,4,7,6,5};
+        int[] target = new int[]{1,2,3,8,0,4,7,6,5};
+    }
+}
