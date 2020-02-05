5d913e3f0d7a91aefc4ed35a77a4bec0b26f3a0d
zhangyibo <goldeneyezhang@163.com>
2020-02-05 21:44:40 +0800

    宽度优先搜索

--- src/com/yibo/WidthSearch.java
+++ src/com/yibo/WidthSearch.java
@@ -2,6 +2,7 @@ package com.yibo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 广度搜索
@@ -11,6 +12,11 @@ public class WidthSearch {
         if (start != null && target != null && start.length == target.length) {
             List<EightDigit> eightDigitList = new ArrayList<>();
             EightDigit eightDigit = EightDigitUtil.iniEightDigit(start);
+            eightDigitList.add(eightDigit);
+            int depth = 0;
+            while (!widthMove(eightDigitList, target, depth)) {
+                depth++;
+            }
         }
     }
 
@@ -18,23 +24,26 @@ public class WidthSearch {
     /**
      * 移动
      *
-     * @param eightDigit
+     * @param eightDigitList
      * @param target
+     * @param depth
      * @return
      */
-    private static Boolean widthMove(EightDigit eightDigit, int[] target, List<EightDigit> eightDigitList) {
+    private static Boolean widthMove(List<EightDigit> eightDigitList, int[] target, int depth) {
         int[] moves = new int[]{3, -3, -1, 1};
-        for (int m : moves) {
-            if (m != eightDigit.getMove() * -1) {
-                int index = eightDigit.getZeroIndex();
-                //可以移动
-                if (index + m >= 0 && index + m < target.length) {
-                    EightDigit newEight = EightDigitUtil.moveZero(eightDigit, m);
-                    if (EightDigitUtil.isTarget(newEight.getNumbers(), target)) {
-                        return true;
-                    } else {
-                        eightDigitList.add(newEight);
-                        return false;
+        for (EightDigit eightDigit : eightDigitList.stream().filter(x -> x.getDepth() == depth).collect(Collectors.toList())) {
+            for (int m : moves) {
+                if (m != eightDigit.getMove() * -1) {
+                    int index = eightDigit.getZeroIndex();
+                    //可以移动
+                    if (index + m >= 0 && index + m < target.length) {
+                        EightDigit newEight = EightDigitUtil.moveZero(eightDigit, m);
+                        if (EightDigitUtil.isTarget(newEight.getNumbers(), target)) {
+                            EightDigitUtil.printRoute(newEight);
+                            return true;
+                        } else {
+                            eightDigitList.add(newEight);
+                        }
                     }
                 }
             }
